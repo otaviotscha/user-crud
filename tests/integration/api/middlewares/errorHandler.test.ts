6dfcc40ada21a45ce53a889e56eb657a427d4ec9
Otávio L. Tschá <otavio.tscha.sting@gmail.com>
2021-11-20 12:29:49 -0300

    refactor: translated api tests to english

--- tests/integration/api/middlewares/errorHandler.test.ts
+++ tests/integration/api/middlewares/errorHandler.test.ts
@@ -2,15 +2,15 @@ import request from 'supertest'
 
 import { startServer, closeServer, runningServer as server } from 'tests/helpers/server'
 
-describe('Error Handler', () => {
+describe('MIDDLEWARES: Error Handler', () => {
   beforeAll(startServer)
 
-  test('deve retornar response com status code 404', async () => {
+  test('should return status code 404', async () => {
     const getResponse = await request(server).get(`/foo`)
     expect(getResponse.statusCode).toBe(404)
   })
 
-  test('deve retornar response com status code 400', async () => {
+  test('should return status code 400', async () => {
     const getResponse = await request(server).get(`/error-sample`)
     expect(getResponse.statusCode).toBe(400)
     expect(getResponse.body.code).toBe(400)
--- tests/integration/api/routes/health-check/controller.test.ts
+++ tests/integration/api/routes/health-check/controller.test.ts
@@ -2,14 +2,14 @@ import request from 'supertest'
 
 import { startServer, closeServer, runningServer as server } from 'tests/helpers/server'
 
-describe('Health Check', () => {
+describe('ROUTES: Health Check', () => {
   beforeAll(startServer)
 
-  test('deve retornar response com status code 204', done => {
+  test('should return status code 204', done => {
     request(server).get('/health-check').expect(204, done)
   })
 
-  test('deve retornar response com status code 404', done => {
+  test('should return status code 404', done => {
     request(server).get('/not-found-route').expect(404, done)
   })
 
