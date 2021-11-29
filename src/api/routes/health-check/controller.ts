cf08630a09c90110acce34ddad07780eafe18757
Otávio L. Tschá <otavio.tscha.sting@gmail.com>
2021-11-29 07:41:14 -0300

    docs: added health-check route description

--- src/api/routes/health-check/controller.ts
+++ src/api/routes/health-check/controller.ts
@@ -1,8 +1,10 @@
 import { Get, HttpCode, JsonController, OnUndefined } from 'routing-controllers'
+import { OpenAPI } from 'routing-controllers-openapi'
 
 @JsonController('/health-check')
 export class HealthCheckController {
   @Get()
+  @OpenAPI({ summary: 'Checks if the API is up' })
   @HttpCode(204)
   @OnUndefined(204)
   getAll() {}
